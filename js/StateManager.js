431bcfa92b143438427553e7c61fb8002c8554de
Olivia Zhang <olivia.ny.zhang@gmail.com>
2016-01-23 02:57:40 -0800

    fix spacing

--- js/StateManager.js
+++ js/StateManager.js
@@ -4,36 +4,36 @@ var RACEGAME = RACEGAME || {};
 ** Constructor for class StateManager
 **/
 RACEGAME.StateManager = function() {
-	this.setStart();
+  this.setStart();
 };
 
 RACEGAME.StateManager.prototype = {
 
-	/**
-	** displays the start menu div
-	**/
-	setStart: function() {
-		$(".state").hide();
-		$("#timer").css({"visibility": "hidden"});
-		$("#start-menu").show();
-		$(".car").css({left: "30px"});
-	},
+  /**
+  ** displays the start menu div
+  **/
+  setStart: function() {
+    $(".state").hide();
+    $("#timer").css({"visibility": "hidden"});
+    $("#start-menu").show();
+    $(".car").css({left: "30px"});
+  },
 
-	/**
-	** displays the main game div and the countdown timer
-	**/
-	setGame: function() {
-		$(".state").hide();
-		$("#game").show();
-		$("#timer").text("").css({"visibility" : "visible"});
-	},
+  /**
+  ** displays the main game div and the countdown timer
+  **/
+  setGame: function() {
+    $(".state").hide();
+    $("#game").show();
+    $("#timer").text("").css({"visibility" : "visible"});
+  },
 
-	/*
-	** displays the finish state
-	**/
-	setFinish: function() {
-		$(".state").hide();
-		$("#timer").css({"visibility" : "hidden"});
-		$("#finish-menu").fadeIn();
-	}
+  /*
+  ** displays the finish state
+  **/
+  setFinish: function() {
+    $(".state").hide();
+    $("#timer").css({"visibility" : "hidden"});
+    $("#finish-menu").fadeIn();
+  }
 };
